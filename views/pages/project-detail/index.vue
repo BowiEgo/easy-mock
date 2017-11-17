2c85488d41b9e6e6bb5a9215eef2e48835d0f042
bowiego <bobosndr@163.com>
2017-11-17 14:18:04 +0800

    delete test form

--- views/pages/project-detail/index.vue
+++ views/pages/project-detail/index.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="em-proj-detail">
-    <form method="POST" action="http://127.0.0.1:7300/api/mock/generateAPI">
-      <input name="project_id" value="5a0d59d0de59b7324dcaaa5a">
-      <button onclick="this.form.submit()"></button>
-    </form>
     <em-header
       icon="cube"
       :title="project.name"
